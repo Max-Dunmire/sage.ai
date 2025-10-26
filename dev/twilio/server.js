3173322c676f1c52cbf9133ac6d51081af734085
Max Dunmire <maxkdunmire@gmail.com>
2025-10-26 08:13:56 -0700

    nothing done

--- dev/twilio/server.js
+++ dev/twilio/server.js
@@ -300,7 +300,7 @@ class MediaStream {
           });
 
           await sleep(5000);
-	  this.isThrottled = false;
+	        this.isThrottled = false;
         }
       } catch (err) {
         console.log(`wtf broken ${err}`)
@@ -387,51 +387,6 @@ class MediaStream {
 
     log("Server: Closed");
   }
-}
-
-
-const MULAW_DECODE_TABLE = (() => {
-  const table = new Int16Array(256);
-  for (let i = 0; i < 256; i++) {
-    let u = ~i & 0xff;
-    let t = ((u & 0x0f) << 3) + 0x84;
-    t <<= (u & 0x70) >> 4;
-    t -= 0x84;
-    table[i] = (u & 0x80) ? (0x84 - t) : (t - 0x84);
-  }
-  return table;
-})();
-
-function decodeMuLawToPCM16(muLawBuf) {
-  const out = new Int16Array(muLawBuf.length);
-  for (let i = 0; i < muLawBuf.length; i++) {
-    out[i] = MULAW_DECODE_TABLE[muLawBuf[i]];
-  }
-  return out;
-}
-
-function pcm16ToWav(pcmBuf, sampleRate) {
-  const numChannels = 1;
-  const bitsPerSample = 16;
-  const byteRate = (sampleRate * numChannels * bitsPerSample) >> 3;
-  const blockAlign = (numChannels * bitsPerSample) >> 3;
-  const dataSize = pcmBuf.length;
-
-  const header = Buffer.alloc(44);
-  header.write("RIFF", 0);
-  header.writeUInt32LE(36 + dataSize, 4);
-  header.write("WAVE", 8);
-  header.write("fmt ", 12);
-  header.writeUInt32LE(16, 16);
-  header.writeUInt16LE(1, 20);
-  header.writeUInt16LE(numChannels, 22);
-  header.writeUInt32LE(sampleRate, 24);
-  header.writeUInt32LE(byteRate, 28);
-  header.writeUInt16LE(blockAlign, 32);
-  header.writeUInt16LE(bitsPerSample, 34);
-  header.write("data", 36);
-  header.writeUInt32LE(dataSize, 40);
-  return Buffer.concat([header, pcmBuf]);
 }
 
 wsserver.listen(HTTP_SERVER_PORT, function () { console.log("Server listening on: http://localhost:%s", HTTP_SERVER_PORT); });
