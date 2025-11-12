c5f5141cd14237fc01df43f086e403e273ff90eb
Max Dunmire <maxkdunmire@gmail.com>
2025-11-12 10:10:52 -0800

    basic Twilio skeleton code

--- sage-server/server.py
+++ sage-server/server.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import FileResponse
 import uvloop
 import asyncio
+import websockets
+
+url = "wss://api.openai.com/v1/realtime?model=gpt-realtime"
+headers = ["Authorization: Bearer " + OPENAI_API_KEY]
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
@@ -15,3 +19,18 @@ def root():
 def switch_to_bidirectional_media_stream():
     return FileResponse(path="./twiml.xml")
 
+@app.websocket('/media')
+async def media(ws: WebSocket):
+    await ws.accept()
+    try:
+        while True:
+            data = await ws.receive_json()
+
+            if data["event"] == "connected":
+                pass
+            if data["event"] == "start":
+                pass
+            if data["event"] == "media":
+                pass
+    except WebSocketDisconnect:
+        print("disconnected")
