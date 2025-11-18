656b4b2927027a790451acd13331c070d30e31be
Max Dunmire <maxkdunmire@gmail.com>
2025-11-17 19:11:39 -0800

    added a logger function

---
+++ sage-server/logging_utils.py
@@ -0,0 +1,45 @@
+import os
+import logging
+
+from datetime import datetime
+
+
+_LOGGER_INITIALIZED = False
+log_level = {
+                "debug": logging.DEBUG, 
+                "info": logging.INFO, 
+                "warning": logging.WARNING,
+                "error": logging.ERROR,
+                "critical": logging.CRITICAL
+            }
+
+def setup_logger(log_dir: str = "logs", level: str = "info"):
+    global log_level
+    global _LOGGER_INITIALIZED
+
+    logger = logging.getLogger("sage")
+    
+    if _LOGGER_INITIALIZED:
+        return logger
+    
+    _LOGGER_INITIALIZED = True
+
+    os.makedirs(log_dir, exist_ok=True)
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_path = os.path.join(log_dir, f"session_{timestamp}.log")
+
+    logger.setLevel(log_level[level])
+
+    handler = logging.FileHandler(log_path)
+    formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
+def make_logger(name: str | None = None):
+    if name is None:
+        return logging.getLogger("sage")
+    return logging.getLogger(f"sage.{name}")
--- sage-server/server.py
+++ sage-server/server.py
@@ -7,10 +7,15 @@ from fastapi.responses import FileResponse
 
 from call_handling import CallHandler
 from settings import settings as env
+from logging_utils import setup_logger, make_logger
 
+setup_logger(level="debug")
+server_logger = make_logger("server")
 
 GPT_REALTIME_URL = env.GPT_REALTIME_URL
 HEADERS = { "Authorization": f"Bearer {env.OPENAI_API_KEY}" }
+
+
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
@@ -19,13 +24,16 @@ app = FastAPI()
 
 @app.post('/twiml')
 def serve_websocket_endpoint():
+    server_logger.debug("twiml endpoint hit")
     return FileResponse(path="./twiml.xml")
 
 @app.websocket('/media')
 async def media(twilio_ws: WebSocket):
     await twilio_ws.accept()
+    server_logger.info("twilio websocket connected")
 
     async with websockets.connect(GPT_REALTIME_URL, additional_headers=HEADERS) as openai_ws:
+        server_logger.info("openai websocket connected")
 
         call_handler = await CallHandler.create(twilio_ws, openai_ws)
 
